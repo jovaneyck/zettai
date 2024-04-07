6782681669ce525ef9222f45b818b44a43b19e2d
Jo Van Eyck <j.vaneyck@farmad.be>
2024-04-07 16:45:03 +0200

    Episode 1: walking skeleton

--- Zettai/Infrastructure.fs
+++
@@ -1,6 +0,0 @@
-﻿module Infrastructure
-
-type Event<'tid, 'tdata> = { AggregateId: 'tid; Data: 'tdata }
-type CommandHandler<'tid, 'tcmd, 'tevent> = 'tcmd -> Event<'tid, 'tevent> list
-type EventStream<'tid, 'tevent> = 'tid -> 'tevent seq
-type EventWriter<'tid, 'tevent> = Event<'tid, 'tevent> -> unit
